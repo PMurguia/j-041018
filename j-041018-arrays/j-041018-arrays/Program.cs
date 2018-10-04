90a6730f65f63e5b89a68a6a7366bafc34fe68da
Peio Murguia <peiomurguia@gmail.com>
2018-10-05 00:03:33 +0200

    Subida noche

--- j-041018-arrays/j-041018-arrays/Program.cs
+++ j-041018-arrays/j-041018-arrays/Program.cs
@@ -47,7 +47,88 @@ namespace j_041018_arrays
             //Usar un for para mostrar los 10 valores en pantalla.
 
 
-          
+            //int sumaNum = 20;
+            //int[] numeros = new int[10];
+            //for (int i = 0; i < numeros.Length; i++)
+            //{
+            //    Console.WriteLine(numeros[i] = sumaNum);
+            //    sumaNum--;
+            //}
+            //Console.ReadLine();
+
+
+
+
+            //int sumaNum1 = 20;
+            //int[] numeros1 = new int[10];
+            //for(int i = 0; i < numeros1.Length; i++)
+            //{
+            //    numeros1[i] = sumaNum1;
+            //    sumaNum1--;
+            //}
+            //for(int i = 0; i< numeros1.Length; i++)
+            //{
+            //    Console.WriteLine(numeros1[i]);
+            //}
+            //Console.ReadLine();
+
+
+            //Crear un array de 3 ints. Usar un for para pedir al usuario que introduzca valores a cada una de las posiciones.
+            //Usar otro for para mostrarlos en pantalla.
+
+            //int[] valors = new int[3];
+            //for (int i = 0; i < valors.Length; i++)
+            //{
+            //    Console.WriteLine("Introduce un valor. ");
+            //    valors[i] = Int32.Parse(Console.ReadLine());
+            //}
+            //Console.WriteLine("Los valores introducidos son: ");
+            //for (int i = 0; i < valors.Length; i++)
+            //{
+
+            //    Console.WriteLine(valors[i]);
+            //}
+            //Console.ReadLine();
+
+
+            //Crear un array de 3 ints. Usar un for para pedir al usuario que introduzca valores a cada una de las posiciones.
+            //Mirar los valores del array una vez tenga los 3 valores y mostrar en pantalla el más pequeño.
+
+            //int[] valors2 = new int[3];
+            //for(int i = 0; i < valors2.Length; i++)
+            //{
+            //    Console.WriteLine("Introduce un valor. ");
+            //    valors2[i] = Int32.Parse(Console.ReadLine());
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("El valor más pequeño es " + valors2.Min());
+            //Console.ReadLine();
+
+
+            //Poner los nombres de los meses en un array de 12. 
+            //Cuando el usuario introduce un número del 1 al 12, mostrar el nombre de ese mes en pantalla.
+
+            //string[] meses = { "enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre" };
+            //int numMes;
+
+            //Console.WriteLine("Introduce un número del 1 al 12. ");
+            //numMes = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine();
+            //Console.WriteLine(meses[numMes]);
+            //Console.ReadLine();
+
+
+
+            //Hacer la media de 10 valores utilizando un array
+
+            //int[] valors3 = { 3, 2, 8, 5, 9, 16, 8, 9, 82, 46 };
+            //Console.WriteLine(valors3.Average());
+            //Console.ReadLine();
+
+
+            //
+
+
         }
     }
 }
