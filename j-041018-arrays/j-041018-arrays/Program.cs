0778df513fe2b97c9216c0abca4a4fb8da2b6a35
Peio Murguia <peiomurguia@gmail.com>
2018-10-04 18:49:30 +0200

    Subida final

---
+++ j-041018-arrays/j-041018-arrays/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace j_041018_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Crear un array de 10 ints. Asignar un valor a cada uno de las 10 posiciones. Mostrar los 10 valores en pantalla.
+
+            //int[] numeros = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }; 
+
+            //for (int i = 0; i < numeros.Length ; i++)
+            //{
+
+            //    Console.WriteLine(numeros[i]);
+            //}
+            //    Console.ReadLine();
+
+
+            //Crear un array de 3 strings. Pedir al usuario 3 veces que escriba un string.
+            //Guardar cada uno de esos strings en la posición 0 , 1 y 2. Mostrar en pantalla en el orden:
+            //primero posición 1, después posición 2 y por último posicion 0.
+
+            //string[] arraystring = new string[3];
+            //Console.WriteLine("Dime 3 palabras. ");
+            //arraystring[0] = Console.ReadLine();
+            //arraystring[1] = Console.ReadLine();
+            //arraystring[2] = Console.ReadLine();
+            //Console.WriteLine();
+
+            //Console.WriteLine(arraystring[1]);
+            //Console.WriteLine(arraystring[2]);
+            //Console.WriteLine(arraystring[0]);
+
+            //Console.ReadLine();
+
+
+
+            // Crear un array de 10 ints. 
+            //Usar un for para asignar los valores de 20 a 11 a las 10 posiciones (en orden descendente).
+            //Usar un for para mostrar los 10 valores en pantalla.
+
+
+          
+        }
+    }
+}
